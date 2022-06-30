822a527983ec011379d91c2916e902e45c828b34
Cole Lyman <cole@colelyman.com>
2022-06-30 00:15:09 -0600

    Change import path to CRISPRessoReport

--- CRISPResso2/CRISPRessoCORE.py
+++ CRISPResso2/CRISPRessoCORE.py
@@ -27,7 +27,7 @@ import traceback
 
 
 from CRISPResso2 import CRISPRessoCOREResources
-from CRISPResso2 import CRISPRessoReport
+from CRISPResso2.CRISPRessoReports import CRISPRessoReport
 from CRISPResso2 import CRISPRessoShared
 from CRISPResso2 import CRISPRessoPlot
 from CRISPResso2 import CRISPResso2Align
