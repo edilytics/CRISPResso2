82d1592883be6238a9553526f48352f1aaa8d8bb
Samuel Nichols <Snic9004@gmail.com>
2023-02-14 08:34:07 -0700

    Fix spacing

--- CRISPResso2/CRISPRessoCOREResources.pyx
+++ CRISPResso2/CRISPRessoCOREResources.pyx
@@ -2,18 +2,24 @@ import cython
 import numpy as np
 cimport numpy as np
 import re
+
 cdef extern from "stdlib.h":
     ctypedef unsigned int size_t
     size_t strlen(char* s)
+
 cdef extern from "Python.h":
     ctypedef void PyObject
     int _PyBytes_Resize(PyObject **, size_t)
     char * PyBytes_AS_STRING(PyObject *)
+
+
 re_find_indels = re.compile("(-*-)")
+
 @cython.boundscheck(False)
 @cython.nonecheck(False)
 @cython.wraparound(False)
 def find_indels_substitutions(read_seq_al, ref_seq_al, _include_indx):
+
     #ref_positions holds the indices for which positions map back to the original reference
     # for example,
     #     1 2 3 4 5 6 7 8
@@ -28,18 +34,21 @@ def find_indels_substitutions(read_seq_al, ref_seq_al, _include_indx):
     substitution_positions=[]
     all_substitution_values=[]
     substitution_values=[]
+
     all_deletion_positions = []
     all_deletion_coordinates = []
     deletion_positions = []
     deletion_coordinates = []
     deletion_sizes = []
     cdef int start_deletion = -1  # the -1 value indicates that there currently isn't a deletion
+
     all_insertion_positions = []
     all_insertion_left_positions = []
     insertion_positions = []
     insertion_coordinates = []
     insertion_sizes = []
     cdef int start_insertion = -1  # the -1 value indicates that there currently isn't an insertion
+
     cdef size_t seq_len = len(ref_seq_al)
     include_indx_set = set(_include_indx)
     nucSet = set(['A', 'T', 'C', 'G', 'N'])
@@ -75,6 +84,7 @@ def find_indels_substitutions(read_seq_al, ref_seq_al, _include_indx):
             if idx > 0 and start_insertion == -1:  # this is the first index of an insertion
                 start_insertion = idx - 1
             current_insertion_size += 1
+
         if read_seq_al[idx_c] == '-' and start_deletion == -1:  # this is the first part of a deletion
             if idx_c - 1 > 0:
                 start_deletion = ref_positions[idx_c]
@@ -89,6 +99,7 @@ def find_indels_substitutions(read_seq_al, ref_seq_al, _include_indx):
                 deletion_coordinates.append((start_deletion, end_deletion))
                 deletion_sizes.append(end_deletion - start_deletion)
             start_deletion = -1
+
     if start_deletion != -1:
         end_deletion = ref_positions[seq_len - 1]
         all_deletion_positions.extend(range(start_deletion, end_deletion))
@@ -97,9 +108,11 @@ def find_indels_substitutions(read_seq_al, ref_seq_al, _include_indx):
             deletion_positions.extend(range(start_deletion, end_deletion))
             deletion_coordinates.append((start_deletion, end_deletion))
             deletion_sizes.append(end_deletion - start_deletion)
+
     cdef size_t substitution_n = len(substitution_positions)
     cdef size_t deletion_n = sum(deletion_sizes)
     cdef size_t insertion_n = sum(insertion_sizes)
+
     return {
         'all_insertion_positions': all_insertion_positions,
         'all_insertion_left_positions': all_insertion_left_positions,
@@ -107,12 +120,14 @@ def find_indels_substitutions(read_seq_al, ref_seq_al, _include_indx):
         'insertion_coordinates': insertion_coordinates,
         'insertion_sizes': insertion_sizes,
         'insertion_n': insertion_n,
+
         'all_deletion_positions': all_deletion_positions,
         'all_deletion_coordinates': all_deletion_coordinates,
         'deletion_positions': deletion_positions,
         'deletion_coordinates': deletion_coordinates,
         'deletion_sizes': deletion_sizes,
         'deletion_n': deletion_n,
+
         'all_substitution_positions': all_substitution_positions,
         'substitution_positions': substitution_positions,
         'all_substitution_values': np.array(all_substitution_values),
@@ -123,12 +138,18 @@ def find_indels_substitutions(read_seq_al, ref_seq_al, _include_indx):
 @cython.boundscheck(False)
 @cython.nonecheck(False)
 @cython.wraparound(False)
+
+
 def find_indels_substitutions_legacy(read_seq_al, ref_seq_al, _include_indx):
+
     cdef char* sub_seq=''
+
     cdef int st
     cdef int en
+
     cdef int idx_c
     cdef int idx
+
     #ref_positions holds the indices for which positions map back to the original reference
     # for example,
     #     1 2 3 4 5 6 7 8
@@ -154,23 +175,29 @@ def find_indels_substitutions_legacy(read_seq_al, ref_seq_al, _include_indx):
                 if idx in _include_indx:
                     substitution_positions.append(idx)
                     substitution_values.append(read_seq_al[idx_c])
+
             idx+=1
+
         else:
             if idx==0:
                 ref_positions.append(-1)
             else:
                 ref_positions.append(-idx)
+
     substitution_n = len(substitution_positions)
+    
     #the remainder of positions are with reference to the original reference sequence indexes we calculated above
     all_deletion_positions=[]
     deletion_positions=[]
     deletion_coordinates=[]
     deletion_sizes=[]
+    
     all_insertion_positions=[]
     all_insertion_left_positions=[]
     insertion_positions=[]
     insertion_coordinates = []
     insertion_sizes=[]
+    
     include_indx_set = set(_include_indx)
     for p in re_find_indels.finditer(read_seq_al):
         st,en=p.span()
@@ -186,7 +213,9 @@ def find_indels_substitutions_legacy(read_seq_al, ref_seq_al, _include_indx):
           deletion_positions.extend(range(ref_st,ref_en))
           deletion_coordinates.append((ref_st,ref_en))
           deletion_sizes.append(en-st)
+    
     deletion_n = np.sum(deletion_sizes)
+    
     for p in re_find_indels.finditer(ref_seq_al):
         st,en=p.span()
         #sometimes insertions run off the end of the reference
@@ -196,6 +225,7 @@ def find_indels_substitutions_legacy(read_seq_al, ref_seq_al, _include_indx):
           continue
         ref_st = ref_positions[st-1]
         ref_en = ref_positions[en]
+        
         all_insertion_left_positions.append(ref_st)
         all_insertion_positions.append(ref_st)
         all_insertion_positions.append(ref_en)
@@ -204,7 +234,9 @@ def find_indels_substitutions_legacy(read_seq_al, ref_seq_al, _include_indx):
           insertion_positions.append(ref_st)
           insertion_positions.append(ref_en)
           insertion_sizes.append(en-st)
+    
     insertion_n = np.sum(insertion_sizes)
+    
     retDict = {
         'all_insertion_positions':all_insertion_positions,
         'all_insertion_left_positions':all_insertion_left_positions,
@@ -213,23 +245,29 @@ def find_indels_substitutions_legacy(read_seq_al, ref_seq_al, _include_indx):
         'insertion_sizes':insertion_sizes,
         'insertion_n':insertion_n,
         'all_deletion_positions':all_deletion_positions,
+        
         'deletion_positions':deletion_positions,
         'deletion_coordinates':deletion_coordinates,
         'deletion_sizes':deletion_sizes,
         'deletion_n':deletion_n,
+        
         'all_substitution_positions':all_substitution_positions,
         'substitution_positions':substitution_positions,
         'all_substitution_values':np.array(all_substitution_values),
         'substitution_values':np.array(substitution_values),
         'substitution_n':substitution_n,
+        
         'ref_positions':ref_positions,
     }
     return retDict
+
+
 def calculate_homology(a, b):
     cdef char *al = a
     cdef char *bl = b
     cdef size_t l = strlen(al)
     cdef float score = 0.0
+    
     for i in range(l):
         if al[i] == bl[i]:
             score+=1
