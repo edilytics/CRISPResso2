eac72d74a41cd38211531cd67e419d3d7200979b
Cole Lyman <cole@colelyman.com>
2024-02-16 14:53:09 -0700

    Extract out get_relative_coordinates function
    This function just computes the relative indexes without doing an alignment.

--- tests/unit_tests/test_CRISPRessoShared.py
+++ tests/unit_tests/test_CRISPRessoShared.py
@@ -21,3 +21,21 @@ def test_get_mismatches():
         -3,
     )
     assert len(mismatch_cords) == 6
+
+
+def test_get_relative_coordinates():
+    s1inds_gap_left, s1inds_gap_right = CRISPRessoShared.get_relative_coordinates('ATCGT', 'TTCGT')
+    assert s1inds_gap_left == [0, 1, 2, 3, 4]
+    assert s1inds_gap_right == [0, 1, 2, 3, 4]
+
+
+def test_get_relative_coordinates_to_gap():
+    s1inds_gap_left, s1inds_gap_right = CRISPRessoShared.get_relative_coordinates('TTC-T', 'ATCGT')
+    assert s1inds_gap_left == [0, 1, 2, 2, 3]
+    assert s1inds_gap_right == [0, 1, 2, 3, 3]
+
+
+def test_get_relative_coordinates_from_gap():
+    s1inds_gap_left, s1inds_gap_right = CRISPRessoShared.get_relative_coordinates('ATCGT', 'TTC-T')
+    assert s1inds_gap_left == [0, 1, 2, 4]
+    assert s1inds_gap_right == [0, 1, 2, 4]
