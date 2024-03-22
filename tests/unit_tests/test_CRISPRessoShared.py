cc6698508e5820c762440b7ee143b5451a1f3dc1
Cole Lyman <cole@colelyman.com>
2024-03-22 13:38:52 -0600

    Uncomment and correct tests for `get_relative_coordinates`

--- tests/unit_tests/test_CRISPRessoShared.py
+++ tests/unit_tests/test_CRISPRessoShared.py
@@ -33,19 +33,19 @@ def test_get_relative_coordinates_to_gap():
     to_sequence = 'TTC-T'
     from_sequence = 'TTCGT'
     s1inds_gap_left, s1inds_gap_right = CRISPRessoShared.get_relative_coordinates(to_sequence, from_sequence)
-    assert s1inds_gap_left == [0, 1, 2, 2, 3] # should this be [0, 1, 2, 2, 4]?
-    assert s1inds_gap_right == [0, 1, 2, 3, 3] # should this be [0, 1, 2, 4, 4]?
+    assert s1inds_gap_left == [0, 1, 2, 2, 4]
+    assert s1inds_gap_right == [0, 1, 2, 4, 4]
     assert from_sequence[0] == to_sequence[s1inds_gap_left[0]]
     assert from_sequence[1] == to_sequence[s1inds_gap_left[1]]
     assert from_sequence[2] == to_sequence[s1inds_gap_left[2]]
-    assert from_sequence[2] == to_sequence[s1inds_gap_left[2]]
-    # assert from_sequence[4] == to_sequence[s1inds_gap_left[4]] # this should accept, right?
+    assert from_sequence[3] == to_sequence[s1inds_gap_left[2]]
+    assert from_sequence[4] == to_sequence[s1inds_gap_left[4]]
 
 
 def test_get_relative_coordinates_start_gap():
     s1inds_gap_left, s1inds_gap_right = CRISPRessoShared.get_relative_coordinates('--CGT', 'TTCGT')
-    assert s1inds_gap_left == [-1, -1, 0, 1, 2] # should this be [-1, -1, 2, 3, 4]?
-    assert s1inds_gap_right == [0, 0, 0, 1, 2] # should this be [2, 2, 2, 3, 4]?
+    assert s1inds_gap_left == [-1, -1, 2, 3, 4]
+    assert s1inds_gap_right == [2, 2, 2, 3, 4]
 
 
 def test_get_relative_coordinates_from_gap():
