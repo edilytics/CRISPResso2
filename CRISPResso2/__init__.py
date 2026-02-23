2688ddec3ed70e8f64acc829da93f5187e38c455
mbowcut2 <mbowcut@gmail.com>
2026-02-23 14:54:42 -0700

    register CRISPRessoPlot under old path as well (backwards compatible)

--- CRISPResso2/__init__.py
+++ CRISPResso2/__init__.py
@@ -1,5 +1,13 @@
 # Backward-compatible re-exports: CRISPRessoPlot and upsetplot moved to
 # CRISPResso2.plots but can still be imported from the top-level package.
 # We can remove this once we update CRISPRessoPro's importing
+import sys
+
 from CRISPResso2.plots import CRISPRessoPlot  # noqa: F401
 from CRISPResso2.plots import upsetplot  # noqa: F401
+
+# Register under old module paths so that `from CRISPResso2.CRISPRessoPlot import X`
+# and `from CRISPResso2.upsetplot import X` still work for downstream consumers
+# (e.g., CRISPRessoPro). Remove once all consumers are updated.
+sys.modules['CRISPResso2.CRISPRessoPlot'] = CRISPRessoPlot
+sys.modules['CRISPResso2.upsetplot'] = upsetplot
